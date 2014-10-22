8096e98d5cb942d418e08a6ad9d0dd0155d63759
有生之年 <huhao.1989.1.29@163.com>
2014-10-22 17:56:46 +0800

    no message

---
+++ NSString-URLEncoding/NSString-URLEncoding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSString-URLEncoding
+//
+//  Created by huhao on 14-10-22.
+//  Copyright (c) 2014年 胡皓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
